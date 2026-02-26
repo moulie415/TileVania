4bf06dd1ea24f4fa53644429cab8c155c3ed4434
Henry Moule <hen10@hotmail.co.uk>
2026-02-26 23:37:56 +0000

    feat: added ladder and climb ladder functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,13 +5,17 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] float runSpeed = 15f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D rb;
     Animator animator;
+    CapsuleCollider2D capsuleCollider;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -20,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -27,10 +31,20 @@ public class PlayerMovement : MonoBehaviour
        moveInput = value.Get<Vector2>();
     }
     
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed && capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            rb.linearVelocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+    
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
         rb.linearVelocity = playerVelocity;
+        
+        animator.SetBool("isRunning", Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon);
     }
     
     void FlipSprite()
@@ -38,5 +52,14 @@ public class PlayerMovement : MonoBehaviour
         if (Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon) {
             transform.localScale = new Vector2(Mathf.Sign(rb.linearVelocity.x), 1f);
         }
+    }
+    
+    void ClimbLadder()
+    {
+        if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            Vector2 climbVelocity = new Vector2(rb.linearVelocity.x, moveInput.y * climbSpeed);
+            rb.linearVelocity = climbVelocity;
+        }   
     }
 }
