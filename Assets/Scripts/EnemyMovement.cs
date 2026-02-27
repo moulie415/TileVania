bf409513b7067965115c1122f244faa9e798a9c3
Henry Moule <henry.moule@zenobe.com>
2026-02-27 23:42:32 +0000

    fix: added missing assets

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       rb.linearVelocity = new Vector2(moveSpeed, 0f);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Trigger exited");
+        moveSpeed = -moveSpeed;
+        transform.localScale = new Vector2(-Mathf.Sign(rb.linearVelocity.x), 1f);
+    }
+}
